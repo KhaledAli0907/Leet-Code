41f143085a800f11e3ef1bc6a3c472ff128d427c
khaledali0907 <khaledali0907@gmail.com>
2024-02-03 21:14:38 +0200

    day3

--- sql50/basic_joins.sql
+++ sql50/basic_joins.sql
@@ -15,3 +15,23 @@ FROM Visits V
 LEFT JOIN Transactions T ON V.visit_id = T.visit_id
 WHERE T.transaction_id IS NULL
 GROUP BY V.customer_id;
+
+
+-- 197. Rising Temperature
+SELECT w2.id FROM Weather AS w1
+JOIN Weather AS w2
+ON w2.temperature > w1.temperature
+AND DATEDIFF(w2.recordDate, w1.recordDate) = 1;
+
+-- 1661. Average Time of Process per Machine
+SELECT a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+FROM activity AS a1
+JOIN activity AS a2
+ON a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id
+AND a1.activity_type="start" AND a2.activity_type = "end"
+GROUP BY a1.machine_id
+
+-- 577. Employee Bonus
+SELECT e.name, b.bonus FROM employee e
+LEFT JOIN bonus b ON e.empId = b.empId
+WHERE IFNULL(bonus, 0) < 1000;
